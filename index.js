4db8ef32005b34826e0eb6bdd38c4f884bb2b918
asha muttinakntimath <asha.mumath@gmail.com>
2017-10-03 17:05:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ return string.toUpperCase()
 function whisher(string){
   return string.toLowerCase()
 }
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
